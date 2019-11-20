a16ca56ff2ffbb027eda53421a7de449fa0e0795
kevbo423 <kevbo423@gmail.com>
2019-11-19 22:31:44 -0500

    Started revising Read_Card.py

--- Read_Card.py
+++ Read_Card.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
-import sys
-import time
 from pirc522 import RFID
 
 reader = RFID()
 
-number = 0
-
 print("Starting Program")
 
 while True:
-	print number
-	number = number + 1
+	#Wait for a tag to be presented
 	reader.wait_for_tag()
+
+	#Request tag
 	(error, tag_type) = reader.request()
 	if not error:
 		print("Tag detected")
+
 		(error, uid) = reader.anticoll()
 		if not error:
 			print("UID: " + str(uid))
@@ -27,6 +25,8 @@ while True:
 				if not reader.card_auth(reader.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
 				  # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 				  print("Reading block 10: " + str(reader.read(10)))
+
+
 				  # Always stop crypto1 when done working
 				  reader.stop_crypto()
 
