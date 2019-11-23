a37623123d94573f8d0a8d8573251b21d9dee377
kevbo423 <kevbo423@gmail.com>
2019-11-22 21:02:51 -0500

    Adjusted location of dump

--- Example_RFID_Read.py
+++ Example_RFID_Read.py
@@ -32,21 +32,24 @@ while run:
 
     (error, uid) = rdr.anticoll()
     if not error:
+
         print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
 
         print("Setting tag")
         util.set_tag(uid)
+
         print("\nAuthorizing")
-        #util.auth(rdr.auth_a, [0x12, 0x34, 0x56, 0x78, 0x96, 0x92])
         util.auth(rdr.auth_b, [0x74, 0x00, 0x52, 0x35, 0x00, 0xFF])
+
         print("\nReading")
         util.read_out(4)
-        print("\nDeauthorizing")
-        util.deauth()
 
         #Print everything from card
         print ("Dumping")
         util.dump()
+
+        print("\nDeauthorizing")
+        util.deauth()
 
         print "Sleeping"
         time.sleep(1)
