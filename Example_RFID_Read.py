f4fddcf7e0dcad6f550af489ce340abf9ab305c6
kevbo423 <kevbo423@gmail.com>
2019-11-22 21:07:49 -0500

    Yep

--- Example_RFID_Read.py
+++ Example_RFID_Read.py
@@ -22,8 +22,6 @@ signal.signal(signal.SIGINT, end_read)
 
 print("Starting")
 while run:
-
-    print("Waiting for card")
     rdr.wait_for_tag()
 
     (error, data) = rdr.request()
@@ -32,27 +30,17 @@ while run:
 
     (error, uid) = rdr.anticoll()
     if not error:
-
-
         print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
 
         print("Setting tag")
         util.set_tag(uid)
-
-        """
         print("\nAuthorizing")
+        #util.auth(rdr.auth_a, [0x12, 0x34, 0x56, 0x78, 0x96, 0x92])
         util.auth(rdr.auth_b, [0x74, 0x00, 0x52, 0x35, 0x00, 0xFF])
-
         print("\nReading")
         util.read_out(4)
-        """
-
-        #Print everything from card
-        print ("Dumping")
         util.dump()
-
         print("\nDeauthorizing")
         util.deauth()
 
-        print "Sleeping"
         time.sleep(1)
