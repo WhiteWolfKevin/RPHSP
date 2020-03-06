025d1402e475864571b2ba388e6ca1995dcf59af
kevbo423 <kevbo423@gmail.com>
2020-03-05 20:38:24 -0500

    Testing with Threading and Arm/Disarm System

--- Home_Security_System.py
+++ Home_Security_System.py
@@ -28,7 +28,7 @@ ROW_PINS = [15, 22, 27, 13] # BCM numbering
 COL_PINS = [18, 14, 17] # BCM numbering
 
 def print_key(key):
-    mylcd.lcd_display_string(str(key), 2, 5)
+    mylcd.lcd_display_string_pos(str(key), 2, 5)
 # -------------------------------------------Keypad Configuration
 
 # Global Variables
