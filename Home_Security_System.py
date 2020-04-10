6de204feb3e6d95e3455567045f35b9fa2d8d2ad
kevbo423 <kevbo423@gmail.com>
2020-04-10 01:11:59 -0400

    Copied Redis version of security system program and started on a new MariaDB version of the program

--- Home_Security_System.py
+++ Home_Security_System.py
@@ -6,91 +6,77 @@ import time
 import os
 import pygame
 import threading
-#import sys
-#import signal
-
-# Import for Redis
-import redis
+import mysql.connector as mariadb
 
 # Global Variables
 alarmSoundLocation = "/home/pi/RPHSP/alarm.mp3"
 
-# Redis server configuration
-redisServer = redis.Redis(host='piserver', port=6379, db=0)
+# MariaDB server configuration
+mariadb_connection = mariadb.connect(host='piserver' user='rphsp', database='rphsp')
+database = mariadb_connection.cursor()
 
 # Set Broadcom mode so we can address GPIO pins by number.
 GPIO.setmode(GPIO.BCM)
 
-# Door Sensor class
-class doorSensor:
-    def __init__(self, name, pin):
-        self.name = name
-        self.pin = pin
-
-# Create array of sensors
-sensors = []
-sensors.append(doorSensor("Front Door", 16))
-sensors.append(doorSensor("Living Room Window", 26))
-sensors.append(doorSensor("Garage Door", 20))
-sensors.append(doorSensor("Basement Door", 21))
-
-# Set up the door sensor pins.
-for sensor in sensors:
-    GPIO.setup(sensor.pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+# Get all GPIO pins for the sensors
+database.execute("select gpio_pin from sensors")
+for gpio_pin in database:
+    print("GPIO Pin: " + gpio_pin)
+    GPIO.setup(gpio_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 # Audio player settings
 pygame.mixer.init()
 pygame.mixer.music.set_volume(1.0)
 
-# Security System Thread
-def securitySystem():
-
-    while True:
-
-        # Grab Alarm Status from Redis server
-        alarmStatus = redisServer.get("alarmStatus")
-
-        # Variables
-        securityBreach = False
-
-        # Print alarm status
-        print(alarmStatus)
-
-        # Check each sensor for a security breach
-        for sensor in sensors:
-            sensor.currentState = GPIO.input(sensor.pin)
-            if (sensor.currentState):
-                # This means the door/window is open
-                securityBreach = True
-                print(sensor.name + " Status: OPEN - WARNING!!!")
-                redisServer.set(sensor.name, "OPEN - WARNING!!!")
-            else:
-                print(sensor.name + " Status: CLOSED")
-                redisServer.set(sensor.name, "CLOSED")
-
-        if (securityBreach and alarmStatus == "Armed"):
-            if (not pygame.mixer.music.get_busy()):
-                pygame.mixer.music.load(alarmSoundLocation)
-                pygame.mixer.music.play(-1)
-        else:
-            if (pygame.mixer.music.get_busy()):
-                pygame.mixer.music.stop()
-
-        # Time delay
-        time.sleep(2)
-        os.system('clear')
-
-# Main Function
-try:
-
-    securitySystemRunning = threading.Thread(target=securitySystem)
-    securitySystemRunning.daemon = True
-    securitySystemRunning.start()
-
-    while True:
-        # Keep Running Application
-        print("Starting sleep...")
-        time.sleep(2)
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+# # Security System Thread
+# def securitySystem():
+#
+#     while True:
+#
+#         # Grab Alarm Status from Redis server
+#         alarmStatus = redisServer.get("alarmStatus")
+#
+#         # Variables
+#         securityBreach = False
+#
+#         # Print alarm status
+#         print(alarmStatus)
+#
+#         # Check each sensor for a security breach
+#         for sensor in sensors:
+#             sensor.currentState = GPIO.input(sensor.pin)
+#             if (sensor.currentState):
+#                 # This means the door/window is open
+#                 securityBreach = True
+#                 print(sensor.name + " Status: OPEN - WARNING!!!")
+#                 redisServer.set(sensor.name, "OPEN - WARNING!!!")
+#             else:
+#                 print(sensor.name + " Status: CLOSED")
+#                 redisServer.set(sensor.name, "CLOSED")
+#
+#         if (securityBreach and alarmStatus == "Armed"):
+#             if (not pygame.mixer.music.get_busy()):
+#                 pygame.mixer.music.load(alarmSoundLocation)
+#                 pygame.mixer.music.play(-1)
+#         else:
+#             if (pygame.mixer.music.get_busy()):
+#                 pygame.mixer.music.stop()
+#
+#         # Time delay
+#         time.sleep(2)
+#         os.system('clear')
+#
+# # Main Function
+# try:
+#
+#     securitySystemRunning = threading.Thread(target=securitySystem)
+#     securitySystemRunning.daemon = True
+#     securitySystemRunning.start()
+#
+#     while True:
+#         # Keep Running Application
+#         print("Starting sleep...")
+#         time.sleep(2)
+#
+# except KeyboardInterrupt:
+#     GPIO.cleanup()
---
+++ REDIS_Home_Security_System.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+# Imports for Original Security System
+import RPi.GPIO as GPIO
+import time
+import os
+import pygame
+import threading
+#import sys
+#import signal
+
+# Import for Redis
+import redis
+
+# Global Variables
+alarmSoundLocation = "/home/pi/RPHSP/alarm.mp3"
+
+# Redis server configuration
+redisServer = redis.Redis(host='piserver', port=6379, db=0)
+
+# Set Broadcom mode so we can address GPIO pins by number.
+GPIO.setmode(GPIO.BCM)
+
+# Door Sensor class
+class doorSensor:
+    def __init__(self, name, pin):
+        self.name = name
+        self.pin = pin
+
+# Create array of sensors
+sensors = []
+sensors.append(doorSensor("Front Door", 16))
+sensors.append(doorSensor("Living Room Window", 26))
+sensors.append(doorSensor("Garage Door", 20))
+sensors.append(doorSensor("Basement Door", 21))
+
+# Set up the door sensor pins.
+for sensor in sensors:
+    GPIO.setup(sensor.pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+# Audio player settings
+pygame.mixer.init()
+pygame.mixer.music.set_volume(1.0)
+
+# Security System Thread
+def securitySystem():
+
+    while True:
+
+        # Grab Alarm Status from Redis server
+        alarmStatus = redisServer.get("alarmStatus")
+
+        # Variables
+        securityBreach = False
+
+        # Print alarm status
+        print(alarmStatus)
+
+        # Check each sensor for a security breach
+        for sensor in sensors:
+            sensor.currentState = GPIO.input(sensor.pin)
+            if (sensor.currentState):
+                # This means the door/window is open
+                securityBreach = True
+                print(sensor.name + " Status: OPEN - WARNING!!!")
+                redisServer.set(sensor.name, "OPEN - WARNING!!!")
+            else:
+                print(sensor.name + " Status: CLOSED")
+                redisServer.set(sensor.name, "CLOSED")
+
+        if (securityBreach and alarmStatus == "Armed"):
+            if (not pygame.mixer.music.get_busy()):
+                pygame.mixer.music.load(alarmSoundLocation)
+                pygame.mixer.music.play(-1)
+        else:
+            if (pygame.mixer.music.get_busy()):
+                pygame.mixer.music.stop()
+
+        # Time delay
+        time.sleep(2)
+        os.system('clear')
+
+# Main Function
+try:
+
+    securitySystemRunning = threading.Thread(target=securitySystem)
+    securitySystemRunning.daemon = True
+    securitySystemRunning.start()
+
+    while True:
+        # Keep Running Application
+        print("Starting sleep...")
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
